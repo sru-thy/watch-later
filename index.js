04762575f260486d4971b14d08325882c09b95f5
Sruthy <56227030+sru-thy@users.noreply.github.com>
2020-10-22 21:33:05 +0530

    add multiple links and cron

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var fs = require('fs');
-var app = require('express')();
-
 var { authorize } = require('./auth');
-var { addItems } = require('./action');
+var { addItems } = require('./function');
+const cron = require('node-cron');
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', (err, content) => {
+task = () => {
+  // Load client secrets from a local file.
+  fs.readFile('client_secret.json', (err, content) => {
     if (err) {
       console.log('Error loading client secret file: ' + err);
       return;
     }
     // Authorize a client with the loaded credentials, then call the YouTube API.
     authorize(JSON.parse(content), addItems);
-});
+  });
+
+}
+
+cron.schedule("0 0 0 * * *", () => {
+  task(); //runs every midnight
+  console.log('again');
+})
--- scrape.js
+++ scrape.js
@@ -2,37 +2,53 @@ const puppeteer = require('puppeteer');
 
 let scrape = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: false,
+      userDataDir: "./user_data",
+      // slowMo: 250
+    });
+
     const page = await browser.newPage();
     await page.goto('https://web.whatsapp.com', { waitUntil: 'domcontentloaded' });
-    page.waitForTimeout(6000)
-    .then(() => console.log('Waited  6 seconds!'));
-  
+    await page.waitForXPath('//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div[2]/span[1]/div', 5000);
+    const element1 = await page.$x('//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div[2]/span[1]/div')
+    if (element1) element1[0].click()
+    await page.waitForXPath('/html/body/div[1]/div/div/div[4]/div/header', 5000);
+    const element2 = await page.$x('/html/body/div[1]/div/div/div[4]/div/header')
+    if (element2) element2[0].click()
+    await page.waitForXPath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[3]/div[1]/div/div/div[2]', 5000);
+    const element3 = await page.$x('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[3]/div[1]/div/div/div[2]')
+    if (element3) element3[0].click()
+    await page.waitForXPath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[1]/button[3]', 5000);
+    const element4 = await page.$x('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[1]/button[3]')
+    if (element4) element4[0].click()
+    await page.waitForXPath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[2]', 5000);
+    const element5 = await page.$x('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[2]', 5000)
+    await element5[0].click()
+    await page.waitForXPath("/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[2]/span/div/div/div/div[5]");
 
-    await page.waitForXPath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div[2]/span/div/div/div')
-    .then(async() => {
-      await page.waitForTimeout(10000)
-      .then(() => console.log('waited 10secs'));
-      }
-    );
-    
     const array = await page.evaluate(() => {
-      let x = document.querySelectorAll('._2o2ZR')
+
+      // Poor choice of variable names ?!, ik :)
+      let x = document.querySelectorAll('._2o2ZR') 
       var y = [...x]
       var z = y.map(p => p.href)
       let a = document.querySelectorAll('._1vUBq')
       var b = [...a]
-      var res = b.filter(q => q.title.includes("youtube.com") || q.title.includes("youtu.be"))
-      var d = res.map(p => p.title)
-      var resu = z.concat(d);
-      var result = resu.map(url => url.replace("http://", "").replace("https://", "").replace("www.", "").replace("youtu.be/", "youtube.com/watch?v=").slice(20))
+      var c = b.filter(q => q.title.includes("youtube.com") || q.title.includes("youtu.be"))
+      var d = c.map(p => p.title)
+      var res = z.concat(d);
+      var result = res.map(url => url.replace("http://", "").replace("https://", "").replace("www.", "").replace("youtu.be/", "youtube.com/watch?v=").slice(20))
       return result;
+
     });
+    
     await browser.close();
     return array;
+
   }
-  catch(error) {
-    console.log('error :(',error.message);
+  catch (error) {
+    console.log('error :(', error.message);
   }
 
 };
